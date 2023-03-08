b012af6357cc995bc36a8341d262539b307c31e9
artificialsi <antonkrytoi228@gmail.com>
2023-03-08 19:54:40 +0100

    Add files via upload

---
+++ replies.py
@@ -0,0 +1,36 @@
+texts = [
+    "A moment pls...",
+    "I am coming, wait a bit...",
+    "I am on my way...",
+    "Hmm.. very interesting",
+    "Be patient please",
+    "I will be here for you in 3 minutes, if i am not - read this again",
+    " :3",
+    " :)"
+]
+
+emojis = [
+    "🤪",
+    "💀",
+    "👻",
+    "😈",
+    "👁👄👁",
+    "😂😂",
+    "😍",
+    "😴"
+]
+
+links = [  # 7
+    "https://www.wikihow.com/Be-Patient",
+    "https://www.wikihow.com/Learn-to-Wait-for-What-You-Want",
+    "https://www.wikihow.com/Use-Your-Time-Wisely",
+    "https://www.wikihow.com/Fall-Asleep-when-You-Can%27t",
+    "https://www.wikihow.com/Meditate"
+]
+
+videos = [  # videoName.mp4
+
+]
+
+# summing up all the replies
+replies = texts + links + emojis
